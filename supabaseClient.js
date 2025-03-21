7c67a47a49121cd4a16ff9d7ed9d7c5d103f3731
Vinildo <v.mondlane1@gmail.com>
2025-03-21 13:21:29 +0200

    Mensagem descritiva sobre a atualização

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelos seus dados do Supabase
-const SUPABASE_URL = 'https://axofggykwirdyjekapns.supabase.co'; // https://financasustm.vercel.app
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4b2ZnZ3lrd2lyZHlqZWthcG5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNDY5MzAsImV4cCI6MjA1NzgyMjkzMH0.aa3wgoH3RfdfOjU9moL8HEWLZMbTNSxxftbCcYmT2E0eyJhbGciOiJIUzI1NiIsInR...'; // Chave Anônima
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-export default supabase;
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://axofggykwirdyjekapns.supabase.co'
+const supabaseKey = process.env.SUPABASE_ANON_KEY; 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4b2ZnZ3lrd2lyZHlqZWthcG5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNDY5MzAsImV4cCI6MjA1NzgyMjkzMH0.aa3wgoH3RfdfOjU9moL8HEWLZMbTNSxxftbCcYmT2E0'
+const supabase = createClient(supabaseUrl, supabaseKey)
